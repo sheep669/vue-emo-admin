0a8008229748f58040cbe7205d87e426021d9222
sheep669 <sheep669@126.com>
2022-08-15 21:41:20 +0800

    新增团长审核ui

---
+++ src/views/groupbuyingorganizer/children/wait-audit.vue
@@ -0,0 +1,79 @@
+<!--
+ * @Author: sheep669
+ * @Description: 待审核tab
+ * @Date: 2022-08-15 21:00:05
+-->
+<template>
+    <div id="waitAudit">
+        waitAudit page
+        <emo-table
+            :config="table_config"
+            :tableData="table_data"
+            :isShow="isShow"
+        >
+        </emo-table>
+    </div>
+</template>
+<script>
+import EmoTable from "@/components/table/index";
+// import constant from "@/constant/api/index";
+export default {
+    name: "WaitAudit",
+    components: {
+        EmoTable,
+    },
+    data() {
+        return {
+            table_data: [],
+            isShow: true,
+            /**
+             * 传递表头配置
+             */
+            table_config: {
+                thead: [
+                    {
+                        label: "会员ID",
+                        prop: "id",
+                        fixed: "left",
+                        width: 70,
+                    },
+                    {
+                        label: "推荐团长",
+                        prop: "recommendGroupBuyingOrganizer",
+                        width: 100,
+                    },
+                    { label: "联系方式", prop: "phoneNumber", width: 120 },
+                    { label: "店铺名称", prop: "storeName", width: 150 },
+                    { label: "提货地址", prop: "receiverAddress", width: 230 },
+                    { label: "推荐人", prop: "referrer", width: 100 },
+                    {
+                        label: "审核状态",
+                        prop: "auditStatus",
+                        type: "function",
+                        callback: (row) => {
+                            const data = this.audit_status.filter(
+                                (item) => item.value == row.auditStatus
+                            );
+                            if (data && data.length > 0) {
+                                return data[0].label;
+                            }
+                        },
+                        width: 100,
+                    },
+                    { label: "申请时间", prop: "applyTime", width: 160 },
+                    {
+                        label: "操作",
+                        width: 150,
+                        type: "slot",
+                        slotName: "operation",
+                        fixed: "right",
+                    },
+                ],
+                checkbox: true,
+            },
+        };
+    },
+};
+</script>
+<style lang='less' scoped>
+</style>    
--- src/views/groupbuyingorganizer/group-buying-organizer-audit.vue
+++ src/views/groupbuyingorganizer/group-buying-organizer-audit.vue
@@ -4,101 +4,50 @@
  * @Date: 2022-7-7 16:30:04
 -->
 <template>
-    <!-- suggest to replace '_'===> '-' -->
-    <div class="group_buying_organizer_audit">
+    <div id="groupBuyingOrganizerAudit">
         <el-card shadow="always" body-style="0px" style="margin: 8px">
-            <div class="filter-form">
-                <el-row>
-                    <el-col :span="18">
-                        <el-form
-                            ref="form"
-                            :model="form"
-                            label-width="100px"
-                            class="emo-form-inline"
-                        >
-                            <el-form-item label="xx姓名">
-                                <el-input
-                                    v-model="form.group_buying_organizer_name"
-                                    placeholder="请输入姓名"
-                                ></el-input>
-                            </el-form-item>
-                            <el-form-item label="xx地址">
-                                <el-input
-                                    v-model="form.receiver_address"
-                                    placeholder="请输入地址"
-                                ></el-input>
-                            </el-form-item>
-                            <el-form-item label="xx联系方式">
-                                <el-input
-                                    v-model="form.phone_number"
-                                    placeholder="请输入手机号"
-                                ></el-input>
-                            </el-form-item>
-                            <el-button type="danger" class="serach"
-                                >搜索</el-button
-                            >
-                        </el-form>
-                    </el-col>
-                    <el-col :span="6">
-                        <div class="pull-right">
-                            <el-button type="danger">添加xx信息</el-button>
-                        </div>
-                    </el-col>
-                </el-row>
-            </div>
-            <emo-table :config="table_config" />
-            <emo-pagination style="margin-top: 15px" />
-            group_buying_organizer_audit page
+            <el-tabs v-model="activeName">
+                <el-tab-pane label="待审核" name="waitAudit"
+                    ><wait-audit v-if="activeName == 'waitAudit'"
+                /></el-tab-pane>
+                <el-tab-pane label="审核中" name="underAudit"
+                    ><under-audit v-if="activeName == 'underAudit'"
+                /></el-tab-pane>
+                <el-tab-pane label="审核通过" name="auditSuccess"
+                    ><audit-success v-if="activeName == 'auditSuccess'"
+                /></el-tab-pane>
+                <el-tab-pane label="审核失败" name="auditError"
+                    ><audit-error v-if="activeName == 'auditError'"
+                /></el-tab-pane>
+            </el-tabs>
         </el-card>
     </div>
 </template>
 <script>
-import EmoTable from "@/components/table/index";
-import EmoPagination from "@/components/pagination/index";
+import WaitAudit from "@/views/groupbuyingorganizer/children/wait-audit.vue";
+import UnderAudit from "@/views/groupbuyingorganizer/children/under-audit.vue";
+import AuditSuccess from "@/views/groupbuyingorganizer/children/audit-success.vue";
+import AuditError from "@/views/groupbuyingorganizer/children/audit-error.vue";
 export default {
     name: "GroupBuyingOrganizerAudit",
     components: {
-        EmoTable,
-        EmoPagination,
+        WaitAudit,
+        UnderAudit,
+        AuditSuccess,
+        AuditError,
     },
     data() {
         return {
-            form: {
-                group_buying_organizer_name: "",
-                receiver_address: "",
-                phone_number: "",
-            },
-            /**
-             * 传递表头配置
-             */
-            table_config: {
-                thead: [
-                    // {label: "会员ID",prop: "user_id",fixed: "left", width: 70},
-                ],
-                checkbox: true,
-                // replace url_param for you need
-                url: `url_param`,
-            },
+            activeName: "waitAudit",
         };
     },
 };
 </script>
 <style lang='less' scoped>
-.group_buying_organizer_audit {
+#groupBuyingOrganizerAudit {
     height: 100%;
     width: calc(100% - 160px);
     float: right;
     overflow-y: auto;
-}
-.emo-form-inline {
-    display: flex;
-    justify-content: space-evenly;
-}
-.pull-right {
-    float: right;
-}
-.serach {
-    height: 40px;
-    margin-left: 12px;
 }
 </style>
